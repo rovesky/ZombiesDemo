5a1ecb619a88177188c8cae782587ef6bb841740
lsj <lsj@lsj.com>
2020-07-29 15:24:19 +0800

    丧尸可以改变动作

--- Assets/Demo/SpawnManager.cs
+++ Assets/Demo/SpawnManager.cs
@@ -21,11 +21,28 @@ public class SpawnManager : MonoBehaviour {
 
     void Start()
     {
-        for(int i = 0; i < gridWidth; i++)
+        var material = Resources.Load<Material>("Zombie1/Zombie1_AttackZombi.animMap");
+
+        var random = new System.Random(100);
+
+        
+        Debug.Log($"material:{material.name}");
+        for (int i = 0; i < gridWidth; i++)
         {
-            for(int j = 0; j < gridHeight; j++)
+            for (int j = 0; j < gridHeight; j++)
             {
-                Instantiate<GameObject>(spawnPrefab, new Vector3(i * 1, 0, j * 1), Quaternion.identity);
+                var randi = (float)random.NextDouble() / 2 + 0.5f;
+                var randj = (float)random.NextDouble() / 2 + 0.5f;
+                GameObject obj = Instantiate<GameObject>(spawnPrefab, new Vector3(i *1+ randi, 0, j *1+ randj), Quaternion.identity);
+
+                if (j == gridHeight - 1)
+                {
+                    var render = obj.GetComponent<MeshRenderer>();
+                    //  Debug.Log($"render.materials[0]:{render.materials[0].name}");
+                    render.materials = new Material[1] { material };
+                    //  Debug.Log($"render.materials[0]1:{render.materials[0].name}");
+                }
+
             }
         }
     }
